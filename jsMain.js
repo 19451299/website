d19d5b308a6f9c41e514331193e767597c53bd4f
19451299 <149350253+19451299@users.noreply.github.com>
2023-11-01 16:36:32 +0800

    comitt

--- jsMain.js
+++ jsMain.js
@@ -3,15 +3,3 @@ console.log("js working");
 // get js and ny talking
 
 console.log();
-
-// shoelace active event
-
-const slCarouselItemActive = this.ShadowRoot.document.querySelector(
-  ".sl_carouselBIG_item"
-);
-
-console.log(slCarouselItemActive);
-
-slCarouselItemActive.addEventListener("sl-slide-change", (ev) => {
-  console.log(ev);
-});
