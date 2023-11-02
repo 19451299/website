fda6defcf431659fc61784f42b5c3d8c6ab87447
19451299 <149350253+19451299@users.noreply.github.com>
2023-11-02 15:35:33 +0800

    commit

--- jsMain.js
+++ jsMain.js
@@ -3,3 +3,19 @@ console.log("js working");
 // get js and ny talking
 
 console.log();
+
+//collapse button toggle
+
+const dropDownButton = document.querySelector(".btn_collapsable");
+const contentToShow = document.querySelector(".btn_collapsable_inside");
+
+dropDownButton.addEventListener("click", (showFn) => {
+  if (contentToShow.style.display === "block") {
+    contentToShow.style.display = "none";
+    // contentToShow.style.maxHeight = null;
+  } else {
+    contentToShow.style.display = "block";
+    // contentToShow.style.maxHeight = contentToShow.scrollHeight + "px";
+  }
+  console.log("fornite battlepass");
+});
